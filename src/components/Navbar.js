ae2e169a349766235e94a973ea419ad482e86723
SimonGideon <simongideon918@gmail.com>
2022-12-06 23:04:18 +0300

    ADD pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import styles from './Navbar.module.css';
 
 const NavbarContainer = () => (
   <nav className={styles.navbar}>
-    <a href="/">Math Magician</a>
+    <a className={styles.header} href="/">Math Magician</a>
     <ul className={styles.pages}>
       <li href="/">Home</li>
       <li href="/Calculator" className={styles.bordered}>Calculator</li>
