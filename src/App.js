b723424fd9138c753d4e085667e73495ce99c848
SimonGideon <simongideon918@gmail.com>
2022-12-06 22:59:55 +0300

    ADD Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
+import NavbarContainer from './components/Navbar';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavbarContainer />
         <Calculator />
       </div>
     );
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const NavbarContainer = () => (
+  <nav className={styles.navbar}>
+    <a href="/">Math Magician</a>
+    <ul className={styles.pages}>
+      <li href="/">Home</li>
+      <li href="/Calculator" className={styles.bordered}>Calculator</li>
+      <li href="/Quote">Quote</li>
+    </ul>
+  </nav>
+);
+
+export default NavbarContainer;
